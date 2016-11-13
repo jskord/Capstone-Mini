632dbcd784044d2e2e86bbd90b8b23e6c3348712
justin skord <jskord@gmail.com>
2016-11-13 13:43:40 -0600

    added create, edit, and delete functions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,44 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: product_id)
     render 'show.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image: params["image"],
+      description: params["directions"]
+    )
+    product.save
+    render 'create.html.erb'
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render 'edit.html.erb'
+  end
+
+  def update
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.name = params["name"]
+    product.price = params["price"]
+    product.image = params["image"]
+    product.description =  params["description"]
+    product.save
+    render 'update.html.erb'
+  end
+
+  def destroy
+    product_id = params[:id]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render 'destroy.html.erb'
+  end
+
 end
