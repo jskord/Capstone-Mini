aff13f56f4b1dfd19b59d2bff54a6f09fd8779e7
justin skord <jskord@gmail.com>
2016-11-16 18:54:28 -0600

    added methods to model folder to show tax etc..

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
 class Product < ApplicationRecord
+  def sale_message
+    if price.to_i < 50 
+      @message = "sale item"
+    else
+      @message = "everyday value"
+    end
+    return @message
+  end
+
+  def tax
+    price.to_i * 0.15
+  end
+
+  def total
+    price.to_i * 1.15
+  end
+
 end
+
+
+
+
+ 
