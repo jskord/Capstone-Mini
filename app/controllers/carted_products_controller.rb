524016d6667c16ff589a2a6fee2f7229fecad0fb
justin skord <jskord@gmail.com>
2016-12-01 20:33:05 -0600

    almost done with cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -12,6 +12,10 @@ class CartedProductsController < ApplicationController
   end
   def index
     @carted_products = current_user.carted_products.where(status: "carted")
-    render 'checkout.html.erb'
+    if @carted_products != nil
+      render 'checkout.html.erb'
+    else
+      redirect_to '/products'
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders, through: :carted_products
   has_many :category_products
+  has_many :carted_products
   has_many :categories, through: :category_products
   has_many :users, through: :carted_products
   
